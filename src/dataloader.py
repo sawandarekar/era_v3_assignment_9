3d0971317e728bf741ad8c0d4ac59a11f12591a5
sawandarekar <sawandarekar@gmail.com>
2024-12-30 17:22:54 +0530

    tiny imagenet data loaded

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,4 +1,5 @@
 import os
+from datasets import load_dataset
 from torch.utils.data import DataLoader
 import multiprocessing  # Import for getting the number of CPU cores
 import torch
@@ -29,6 +30,48 @@ def get_test_transformers():
                                        ])
     return test_transforms
 
+
+def get_tiny_imagenet_dataset(batch_size):
+    dataset = load_dataset("Maysee/tiny-imagenet")
+    print(f"Dataset keys: {dataset.keys()}")
+ 
+    transform_train = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.Grayscale(num_output_channels=3),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+ 
+    transform_val = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.Grayscale(num_output_channels=3),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+ 
+    class TinyImageNetDataset:
+        def __init__(self, dataset_split, transform):
+            self.data = dataset_split
+            self.transform = transform
+ 
+        def __len__(self):
+            return len(self.data)
+ 
+        def __getitem__(self, idx):
+            image = self.data[idx]["image"]
+            label = self.data[idx]["label"]
+            if self.transform:
+                image = self.transform(image)
+            return image, label
+ 
+    train_loader = DataLoader(
+        TinyImageNetDataset(dataset["train"], transform_train), batch_size=batch_size, shuffle=True
+    )
+    val_loader = DataLoader(
+        TinyImageNetDataset(dataset["valid"], transform_val), batch_size=batch_size, shuffle=False
+    )
+    return train_loader, val_loader
     
 def get_MNIST_data_loader():
     train = datasets.MNIST('./data', train=True, download=True, transform=get_train_transformers())
--- src/main.py
+++ src/main.py
@@ -6,25 +6,28 @@ from train import train
 from validate import test
 import torch.optim as optim
 from torchsummary import summary
-from dataloader import get_MNIST_data_loader
+from dataloader import get_tiny_imagenet_dataset
 from checkpoint import save_model
+import config
+
+# train dataloader
+train_loader, test_loader = get_tiny_imagenet_dataset(config.BATCH_SIZE)
+# num_classes = len(train_loader.classes)
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
-model = ResNet50_Model().to(device)
-summary(model, input_size=(1, 28, 28))
-
-
-# train dataloader
-train_loader, test_loader = get_MNIST_data_loader()
+model = ResNet50_Model(200).to(device)
+summary(model, input_size=(3, 28, 28))
 
 device = torch.device("mps") if torch.backends.mps.is_available() else "cpu"
 
-model =  ResNet50_Model().to(device)
+model =  ResNet50_Model(200).to(device)
 optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
 scheduler = StepLR(optimizer, step_size=6, gamma=0.1)
 
 criterion = nn.CrossEntropyLoss()
+
+
 
 EPOCHS = 20
 for epoch in range(EPOCHS):
